2d775c8dcee8988bf8cd2cca1e03e1d6d8e777d4
kablaa <kablaa@hackmy.world>
2018-02-22 22:11:14 +0000

    ok now it really works

--- get_creds.sh
+++ get_creds.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 username="$1"
 read -p "Enter Password: " -s password
 echo 
 read -p "Re-Enter Password: " -s rePassword
 echo 
-if ["$password" == "$rePassword"]
-then
+if [ "$password" == "$rePassword" ]; then
     mkdir auth
     docker run --entrypoint htpasswd registry:2 -Bbn "$username" "$password" > auth/htpasswd
     echo "Password for user $username successfully set"
